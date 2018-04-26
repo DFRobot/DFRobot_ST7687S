aca97d0f0209a17565b9af2a575aecc652fb12a9
forKnowYou <1003425554@qq.com>
2018-04-26 12:21:56 +0800

    update comment

--- examples/DFRobot_ST7687S_Latch_Demo/DFRobot_ST7687S_Latch_Demo.ino
+++ examples/DFRobot_ST7687S_Latch_Demo/DFRobot_ST7687S_Latch_Demo.ino
@@ -46,12 +46,12 @@ void loop(void)
   tft.fillScreen(DISPLAY_WHITE);
 
   tft.fillScreen(DISPLAY_BLACK);
-  tft.drawCircle(0, 0, 20, DISPLAY_GREEN);  //draw circle
-  tft.drawRect(-20, -20, 40, 40, DISPLAY_CYAN);  //draw rectangle
-  tft.drawLine(-64, -64, 64, 64, DISPLAY_RED);  //draw line
-  tft.drawHLine(-64, 0, 128, DISPLAY_WHITE);  //draw horizontal line
-  tft.drawVLine(0, -64, 128, DISPLAY_WHITE);  //draw vertical line
-  tft.drawTriangle(-20, -50, 0, 0, 50, 20, DISPLAY_ORANGE);  //draw triangle
+  tft.drawCircle(0, 0, 20, DISPLAY_GREEN);  //draw circle at (0, 0) and radius = 20
+  tft.drawRect(-20, -30, 40, 60, DISPLAY_CYAN);  //draw rectangle at (-20, -30), width = 40, height = 60
+  tft.drawLine(-64, -64, 64, 64, DISPLAY_RED);  //draw line at (-64, -64) to (64, 64)
+  tft.drawHLine(-64, 0, 128, DISPLAY_WHITE);  //draw horizontal line at (-64, 0), length = 128
+  tft.drawVLine(0, -64, 128, DISPLAY_WHITE);  //draw vertical line at (0, -128), length = 128
+  tft.drawTriangle(-20, -50, 0, 0, 50, 20, DISPLAY_ORANGE);  //draw triangle at (-20, -50), (0, 0), (50, 20)
   delay(1000);
   tft.fillCircle(0, 0, 20, DISPLAY_GREEN);  //fill circle
   delay(500);
@@ -63,7 +63,7 @@ void loop(void)
   tft.setTextColor(DISPLAY_WHITE);  //set text color to white
   tft.setTextBackground(DISPLAY_BLACK);  //set text background to black
   tft.setTextSize(2);  //2 * text size, default text size: 6 * 8
-  tft.setCursor(tft.width / 2, tft.height / 2);
+  tft.setCursor(tft.width / 2, tft.height / 2);  //set text position to center
   tft.print("fire");
   delay(1000);
   tft.setCursor(tft.width / 2, tft.height / 2);
